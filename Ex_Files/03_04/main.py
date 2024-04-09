b844b8d7b374e4d75cc8c5c9e302313ea8b67bab
David RG <davidromerog@gmail.com>
2024-04-09 19:28:19 +0800

    changes during the training

--- Ex_Files/03_04/main.py
+++ Ex_Files/03_04/main.py
@@ -25,10 +25,11 @@ with open("laureates.csv", "r") as f:
 #      hey[2] == "y"
 # 2. You can add to a list using
 #      my_list.append("something")
-
 laureates_beginning_with_a = []
 # LinkedIn learner code here
-
+for laureate in laureates:
+    if laureate["name"][0] == "A":
+        laureates_beginning_with_a.append(laureate)
 
 with open("laureates.json", "w") as f:
     json.dump(laureates_beginning_with_a, f, indent=2)
--- Ex_Files/04_01_end/main.py
+++ Ex_Files/04_01_end/main.py
@@ -6,5 +6,8 @@ response = requests.get(
 last_twenty_years = response.json()[1][:20]
 
 for year in last_twenty_years:
+    #DRG To avoid an error, we need to check if the value is None (I get an error on the year 2023)
+    if year["value"] is None:
+        year["value"] = 0
     display_width = year["value"] // 10_000_000
     print(f'{year["date"]}: {year["value"]}', "=" * display_width)
